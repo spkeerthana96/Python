bee79932df4f058818cfce0c114d94153d3561c9
spkeerthana96 <36473072+spkeerthana96@users.noreply.github.com>
2018-02-14 15:23:21 +0530

    Add files via upload

---
+++ 2 max among 3 elements.py
@@ -0,0 +1,20 @@
+def max_of_three(a,b,c):
+
+    if a>b and a>c:
+
+        return a
+
+    elif b>c:
+
+        return b
+
+    else:
+        return c
+a=input()
+
+b=input()
+
+c=input()
+d=max_of_three(int(a),int(b),int(c))
+
+print("Maximum among "+a+","+b+","+c+" is "+str(d))
---
+++ 3 Find Length of string.py
@@ -0,0 +1,7 @@
+def length(val):
+    cnt=0
+    for i in val:
+        cnt+=1
+    print("Length of is %d" % cnt)
+length("1234")
+length([1,2,3])
